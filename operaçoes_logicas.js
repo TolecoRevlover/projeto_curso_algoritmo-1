d05d15eccf99ab732aafd789b8be9335ee3319ed
TolecoRevlover <lh9661@gmail.com>
2021-02-06 15:00:42 -0300

    Primeiro commit do código

---
+++ operaçoes_logicas.js
@@ -0,0 +1,17 @@
+var nome, nota1, nota2, passou;
+
+passou = false;
+
+    nome = prompt("Digite o nome do aluno: ")
+    nota1 = prompt("Digite a nota 1 do aluno: ")
+    nota2 = prompt("Digite a nota 2 do aluno: ")
+
+    media = (parseInt(nota1) + parseInt(nota2)) / 2;
+
+    if(media >= 50)
+        passou = true;
+
+    if(passou &&(media >=70 && media <=90))
+        alert("Aprovado!"+ nome)
+    else
+        alert("Reprovado"+ nome)
